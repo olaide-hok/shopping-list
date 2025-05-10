7ae61e6d5c2bf64f6f34e0c49c0af681ec43e7a1
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-10 16:54:22 +0100

    create reset password screen

---
+++ app/(auth)/reset-password.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ResetScreen = () => {
+    return (
+        <View>
+            <Text>Reset Password Screen</Text>
+        </View>
+    );
+};
+
+export default ResetScreen;
+
+const styles = StyleSheet.create({});
