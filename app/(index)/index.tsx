51b58d9c4d621a19996a704f7d7aa67d1202e0e2
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-10 16:54:43 +0100

    setup home screen

---
+++ app/(index)/index.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import {ThemedText} from '@/components/ThemedText';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <ThemedText type="title">Sign In</ThemedText>
+        </View>
+    );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
