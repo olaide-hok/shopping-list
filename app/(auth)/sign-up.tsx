c7dadc1ebc4b8c15a8bd2990a376faac464701a8
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-10 16:51:03 +0100

    setup sign-up screen

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {ThemedText} from '@/components/ThemedText';
+
+const SignUpScreen = () => {
+    return (
+        <View>
+            <ThemedText type="title">Sign Up</ThemedText>
+        </View>
+    );
+};
+
+export default SignUpScreen;
+
+const styles = StyleSheet.create({});
