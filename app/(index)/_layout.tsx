54254d776bead41868093e9e5045ee8517bca210
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-11 18:00:01 +0100

    wrap layout with ListCreationProvider
    stack color-picker, emoji-picker, and list (create, scan, and new) screens

--- app/(index)/_layout.tsx
+++ app/(index)/_layout.tsx
@@ -1,29 +1,93 @@
-import {Stack} from 'expo-router';
+import Button from '@/components/ui/button';
+import {ListCreationProvider} from '@/context/ListCreationContext';
+import {useUser} from '@clerk/clerk-expo';
+import {Redirect, Stack, useRouter} from 'expo-router';
 
 export default function HomeRoutesLayout() {
+    const router = useRouter();
+    const {user} = useUser();
+
+    if (!user) {
+        return <Redirect href={'/(auth)'} />;
+    }
+
     return (
-        <Stack
-            screenOptions={{
-                ...(process.env.EXPO_OS !== 'ios'
-                    ? {}
-                    : {
-                          headerLargeTitle: true,
-                          headerTransparent: true,
-                          headerBlurEffect: 'systemChromeMaterial',
-                          headerLargeTitleShadowVisible: false,
-                          headerShadowVisible: true,
-                          headerLargeStyle: {
-                              // NEW: Make the large title transparent to match the background.
-                              backgroundColor: 'transparent',
-                          },
-                      }),
-            }}>
-            <Stack.Screen
-                name="index"
-                options={{
-                    headerTitle: 'Shopping Lists',
-                }}
-            />
-        </Stack>
+        <ListCreationProvider>
+            <Stack
+                screenOptions={{
+                    ...(process.env.EXPO_OS !== 'ios'
+                        ? {}
+                        : {
+                              headerLargeTitle: true,
+                              headerTransparent: true,
+                              headerBlurEffect: 'systemChromeMaterial',
+                              headerLargeTitleShadowVisible: false,
+                              headerShadowVisible: true,
+                              headerLargeStyle: {
+                                  // NEW: Make the large title transparent to match the background.
+                                  backgroundColor: 'transparent',
+                              },
+                          }),
+                }}>
+                <Stack.Screen
+                    name="index"
+                    options={{
+                        headerTitle: 'Shopping Lists',
+                    }}
+                />
+                <Stack.Screen
+                    name="list/new/index"
+                    options={{
+                        presentation: 'formSheet',
+                        sheetGrabberVisible: true,
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    name="profile"
+                    options={{
+                        presentation: 'formSheet',
+                        sheetAllowedDetents: [0.75, 1],
+                        sheetGrabberVisible: true,
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    name="list/new/scan"
+                    options={{
+                        presentation: 'fullScreenModal',
+                        headerLargeTitle: false,
+                        headerTitle: 'Scan QR code',
+                        headerLeft: () => (
+                            <Button
+                                variant="ghost"
+                                onPress={() => router.back()}>
+                                Cancel
+                            </Button>
+                        ),
+                    }}
+                />
+                <Stack.Screen
+                    name="emoji-picker"
+                    options={{
+                        presentation: 'formSheet',
+                        headerLargeTitle: false,
+                        headerTitle: 'Choose an emoji',
+                        sheetAllowedDetents: [0.5, 0.75, 1],
+                        sheetGrabberVisible: true,
+                    }}
+                />
+                <Stack.Screen
+                    name="color-picker"
+                    options={{
+                        presentation: 'formSheet',
+                        headerLargeTitle: false,
+                        headerTitle: 'Choose a color',
+                        sheetAllowedDetents: [0.5, 0.75, 1],
+                        sheetGrabberVisible: true,
+                    }}
+                />
+            </Stack>
+        </ListCreationProvider>
     );
 }
