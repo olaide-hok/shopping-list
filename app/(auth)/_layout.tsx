d0036589fd469e09358b05b4a5b70e4c7078cb6d
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-11 08:12:39 +0100

    add screenOptions prop to Stack

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -8,7 +8,22 @@ export default function AuthRoutesLayout() {
     if (isSignedIn) return <Redirect href="/(index)/index" />;
 
     return (
-        <Stack>
+        <Stack
+            screenOptions={{
+                ...(process.env.EXPO_OS !== 'ios'
+                    ? {}
+                    : {
+                          headerLargeTitle: true,
+                          headerTransparent: true,
+                          headerBlurEffect: 'systemChromeMaterial',
+                          headerLargeTitleShadowVisible: false,
+                          headerShadowVisible: true,
+                          headerLargeStyle: {
+                              // NEW: Make the large title transparent to match the background.
+                              backgroundColor: 'transparent',
+                          },
+                      }),
+            }}>
             <Stack.Screen name="index" options={{headerTitle: 'Sign in'}} />
             <Stack.Screen name="sign-up" options={{headerTitle: 'Sign up'}} />
             <Stack.Screen
