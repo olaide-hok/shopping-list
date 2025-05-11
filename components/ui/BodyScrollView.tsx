92a343ca271dc479b7df33c56ff4cafb0b10f90f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-11 08:11:50 +0100

    setup reusable BodyScrollView component

---
+++ components/ui/BodyScrollView.tsx
@@ -0,0 +1,17 @@
+import {forwardRef} from 'react';
+import {ScrollView, ScrollViewProps} from 'react-native';
+
+export const BodyScrollView = forwardRef<any, ScrollViewProps>((props, ref) => {
+    return (
+        <ScrollView
+            automaticallyAdjustsScrollIndicatorInsets
+            contentInsetAdjustmentBehavior="automatic"
+            contentInset={{bottom: 0}}
+            scrollIndicatorInsets={{bottom: 0}}
+            {...props}
+            ref={ref}
+        />
+    );
+});
+
+BodyScrollView.displayName = 'BodyScrollView';
