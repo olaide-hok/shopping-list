2cc37f95c72d50cb607c3b0641149140d142ae88
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-11 09:47:45 +0100

    complete onSignInPress function and render error message

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,21 +1,55 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import React, {useCallback, useState} from 'react';
 import {ThemedText} from '@/components/ThemedText';
-import {useSignIn} from '@clerk/clerk-expo';
-import {Link, useRouter} from 'expo-router';
+import {isClerkAPIResponseError, useSignIn} from '@clerk/clerk-expo';
+import {useRouter} from 'expo-router';
 import {BodyScrollView} from '@/components/ui/BodyScrollView';
 import TextInput from '@/components/ui/text-input';
 import Button from '@/components/ui/button';
+import {ClerkAPIError} from '@clerk/types';
 
 const SignInScreen = () => {
     const {signIn, setActive, isLoaded} = useSignIn();
     const router = useRouter();
 
-    const [emailAdress, setEmailAddress] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
-    const [isSignedIn, setIsSignedIn] = useState(false);
+    const [isSigningIn, setIsSigningIn] = useState(false);
+    const [errors, setErrors] = useState<ClerkAPIError[]>([]);
 
-    const onSignInPress = () => {};
+    // Handle the submission of the sign-in form
+    const onSignInPress = useCallback(async () => {
+        if (!isLoaded) return;
+        setIsSigningIn(true);
+
+        // Start the sign-in process using the email and password provided
+        try {
+            const signInAttempt = await signIn.create({
+                identifier: emailAddress,
+                password,
+            });
+
+            // If sign-in process is complete, set the created session as active
+            // and redirect the user
+            if (signInAttempt.status === 'complete') {
+                await setActive({session: signInAttempt.createdSessionId});
+                router.replace('/(index)');
+            } else {
+                // If the status isn't complete, check why. User might need to
+                // complete further steps.
+                console.error(JSON.stringify(signInAttempt, null, 2));
+            }
+        } catch (err) {
+            // See https://clerk.com/docs/custom-flows/error-handling
+            // for more info on error handling
+            console.error(JSON.stringify(err, null, 2));
+            if (isClerkAPIResponseError(err)) {
+                setErrors(err.errors);
+            }
+        } finally {
+            setIsSigningIn(false);
+        }
+    }, [isLoaded, emailAddress, password]);
 
     return (
         <BodyScrollView
@@ -23,7 +57,7 @@ const SignInScreen = () => {
                 padding: 16,
             }}>
             <TextInput
-                value={emailAdress}
+                value={emailAddress}
                 label="Email"
                 placeholder="Enter email"
                 autoCapitalize="none"
@@ -39,10 +73,16 @@ const SignInScreen = () => {
             />
             <Button
                 onPress={onSignInPress}
-                loading={isSignedIn}
-                disabled={!emailAdress || !password || isSignedIn}>
+                loading={isSigningIn}
+                disabled={!emailAddress || !password || isSigningIn}>
                 Sign in
             </Button>
+
+            {errors.map((error) => (
+                <ThemedText key={error.longMessage} style={{color: 'red'}}>
+                    {error.longMessage}
+                </ThemedText>
+            ))}
 
             <View style={{marginTop: 16, alignItems: 'center'}}>
                 <ThemedText>Don&apos;t have an account?</ThemedText>
