cec4c328fba0ae952daf17e2babb1ecc109970ce
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-11 09:46:01 +0100

    add sign out feature

--- app/(index)/index.tsx
+++ app/(index)/index.tsx
@@ -1,12 +1,17 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import React from 'react';
 import {ThemedText} from '@/components/ThemedText';
+import {BodyScrollView} from '@/components/ui/BodyScrollView';
+import Button from '@/components/ui/button';
+import {useClerk} from '@clerk/clerk-expo';
 
 const HomeScreen = () => {
+    const {signOut} = useClerk();
     return (
-        <View>
-            <ThemedText type="title">Sign In</ThemedText>
-        </View>
+        <BodyScrollView contentContainerStyle={{padding: 16}}>
+            <ThemedText type="title">Home In</ThemedText>
+            <Button onPress={signOut}>Sign out</Button>
+        </BodyScrollView>
     );
 };
 
