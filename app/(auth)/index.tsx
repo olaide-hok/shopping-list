8284c34c52b1ddf66494450736b429612cfcb253
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-11 08:15:31 +0100

    update SignInScreen

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -3,6 +3,9 @@ import React, {useState} from 'react';
 import {ThemedText} from '@/components/ThemedText';
 import {useSignIn} from '@clerk/clerk-expo';
 import {Link, useRouter} from 'expo-router';
+import {BodyScrollView} from '@/components/ui/BodyScrollView';
+import TextInput from '@/components/ui/text-input';
+import Button from '@/components/ui/button';
 
 const SignInScreen = () => {
     const {signIn, setActive, isLoaded} = useSignIn();
@@ -12,11 +15,51 @@ const SignInScreen = () => {
     const [password, setPassword] = useState('');
     const [isSignedIn, setIsSignedIn] = useState(false);
 
+    const onSignInPress = () => {};
+
     return (
-        <View>
-            <ThemedText type="title">Sign In</ThemedText>
-            <Link href={'/sign-up'}> Go to Sign Up</Link>
-        </View>
+        <BodyScrollView
+            contentContainerStyle={{
+                padding: 16,
+            }}>
+            <TextInput
+                value={emailAdress}
+                label="Email"
+                placeholder="Enter email"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                onChangeText={setEmailAddress}
+            />
+            <TextInput
+                value={password}
+                label="Password"
+                placeholder="Enter password"
+                secureTextEntry={true}
+                onChangeText={setPassword}
+            />
+            <Button
+                onPress={onSignInPress}
+                loading={isSignedIn}
+                disabled={!emailAdress || !password || isSignedIn}>
+                Sign in
+            </Button>
+
+            <View style={{marginTop: 16, alignItems: 'center'}}>
+                <ThemedText>Don&apos;t have an account?</ThemedText>
+                <Button onPress={() => router.push('/sign-up')} variant="ghost">
+                    Sign up
+                </Button>
+            </View>
+
+            <View style={{marginTop: 16, alignItems: 'center'}}>
+                <ThemedText>Forgot password?</ThemedText>
+                <Button
+                    onPress={() => router.push('/reset-password')}
+                    variant="ghost">
+                    Reset password
+                </Button>
+            </View>
+        </BodyScrollView>
     );
 };
 
