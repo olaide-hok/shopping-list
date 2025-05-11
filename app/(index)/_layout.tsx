afe56b4001b6751fea8fb79316775fd8bb643591
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-11 08:12:46 +0100

    add screenOptions prop to Stack

--- app/(index)/_layout.tsx
+++ app/(index)/_layout.tsx
@@ -2,7 +2,22 @@ import {Stack} from 'expo-router';
 
 export default function HomeRoutesLayout() {
     return (
-        <Stack>
+        <Stack
+            screenOptions={{
+                ...(process.env.EXPO_OS !== 'ios'
+                    ? {}
+                    : {
+                          headerLargeTitle: true,
+                          headerTransparent: true,
+                          headerBlurEffect: 'systemChromeMaterial',
+                          headerLargeTitleShadowVisible: false,
+                          headerShadowVisible: true,
+                          headerLargeStyle: {
+                              // NEW: Make the large title transparent to match the background.
+                              backgroundColor: 'transparent',
+                          },
+                      }),
+            }}>
             <Stack.Screen name="index" />
         </Stack>
     );
