bddedb4a28ec435efaf588fb848f3b0acfe60dda
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-12 15:52:44 +0100

    update HomeScreen

--- app/(index)/index.tsx
+++ app/(index)/index.tsx
@@ -1,17 +1,52 @@
-import {StyleSheet} from 'react-native';
+import {Pressable, StyleSheet} from 'react-native';
 import React from 'react';
 import {ThemedText} from '@/components/ThemedText';
 import {BodyScrollView} from '@/components/ui/BodyScrollView';
 import Button from '@/components/ui/button';
 import {useClerk} from '@clerk/clerk-expo';
+import {Stack, useRouter} from 'expo-router';
+import {IconSymbol} from '@/components/ui/IconSymbol';
+import {appleBlue} from '@/constants/Colors';
 
 const HomeScreen = () => {
+    const router = useRouter();
     const {signOut} = useClerk();
+
+    const renderHeaderRight = () => {
+        return (
+            <Pressable
+                onPress={() => {
+                    router.push('/list/new');
+                }}>
+                <IconSymbol name="plus" color={appleBlue} />
+            </Pressable>
+        );
+    };
+
+    const renderHeaderLeft = () => {
+        return (
+            <Pressable
+                onPress={() => {
+                    router.push('/profile');
+                }}>
+                <IconSymbol name="gear" color={appleBlue} />
+            </Pressable>
+        );
+    };
+
     return (
-        <BodyScrollView contentContainerStyle={{padding: 16}}>
-            <ThemedText type="title">Home In</ThemedText>
-            <Button onPress={signOut}>Sign out</Button>
-        </BodyScrollView>
+        <>
+            <Stack.Screen
+                options={{
+                    headerRight: renderHeaderRight,
+                    headerLeft: renderHeaderLeft,
+                }}
+            />
+            <BodyScrollView contentContainerStyle={{padding: 16}}>
+                <ThemedText type="title">Home In</ThemedText>
+                <Button onPress={signOut}>Sign out</Button>
+            </BodyScrollView>
+        </>
     );
 };
 
